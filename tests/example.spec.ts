017972e85a4713c679be0656c8123d50073923c3
Alexandre Neto <alexandreneto.dev@gmail.com>
2023-12-16 14:38:55 -0300

    Feature - Add new workflow

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,7 +1,8 @@
 import { expect, test } from '@playwright/test';
 
+
 test('has title', async ({ page }) => {
-  await page.goto('http://localhost:4200');
+  await page.goto('http://localhost:4200'); // Replace with your Angular app's URL
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Angular Calculator/);
