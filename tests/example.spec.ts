1f1b38cd2c7122e8853765fe919d7b5c566afa3c
Alexandre Neto <alexandreneto.dev@gmail.com>
2023-12-16 15:18:08 -0300

    Update example.spec.ts

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -5,7 +5,13 @@ test('has title', async ({ page }) => {
   await page.goto('http://localhost:4200'); // Replace with your Angular app's URL
 
   // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Angular Calculator/);
+  await page.waitForSelector('body');
+
+  const textoDaPagina = await page.textContent('body');
+
+  const palavraDesejada = 'Angular Calculator';
+
+  expect(textoDaPagina.toLowerCase()).toContain(palavraDesejada.toLowerCase());
 });
 
 // test('get started link', async ({ page }) => {
